3e9cc0b9f678b945914ab98344a349e053df28d6
Guillermo Garcia <bitcoder@gmail.com>
2021-04-06 05:10:13 -0500

    First commit from unzipped file

---
+++ README.md
@@ -0,0 +1,25 @@
+# Vue AWS SAM simple Example
+
+## Project setup
+
+You will need to install [npm](https://www.npmjs.com/) and [AWS SAM](https://aws.amazon.com/serverless/sam/) first.
+
+Then, you can get everything set up with a simple install command in the project directory:
+
+```
+npm install
+```
+
+### Serving
+
+Once you've set up the project, you can run the front and back apps with the serve command:
+
+```
+npm run serve
+```
+
+### Running the unit tests
+
+```
+npm run test
+```
---
+++ front/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Welcome to the challenge app</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+  message = 'Loading...'
+
+  async mounted() {
+    const res = await fetch('http://localhost:3000/hello');
+    if (res.ok) {
+      const { message } = await res.json();
+      this.message = message;
+    }
+  }
+}
+</script>
